9286988b0af5f85710faeab5d26c7956d099a9e6
Andrej Kudinov <kudinov.andrej@list.ru>
2023-02-05 13:09:08 +0300

    добавил отзывы

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// функции скролла
 document.addEventListener('click', function () {
     const buttonScrollExscursionLeft = document.querySelector('.scroll-button-left');
     buttonScrollExscursionLeft.onclick = function () {
@@ -11,5 +10,21 @@ document.addEventListener('click', function () {
     const buttonScrollExscursionRight = document.querySelector('.scroll-button-right');
     buttonScrollExscursionRight.onclick = function () {
         document.querySelector('.excursions__list').scrollLeft += 50;
+    };
+}, false);
+
+
+document.addEventListener('click', function () {
+    const buttonScrollExscursionLeft = document.querySelector('.scroll_type_review-left');
+    buttonScrollExscursionLeft.onclick = function () {
+        document.querySelector('.reviews__conteiner').scrollLeft += -50;
+    };
+}, false);
+
+
+document.addEventListener('click', function () {
+    const buttonScrollExscursionRight = document.querySelector('.scroll_type_review-right');
+    buttonScrollExscursionRight.onclick = function () {
+        document.querySelector('.reviews__conteiner').scrollLeft += 50;
     };
 }, false);
